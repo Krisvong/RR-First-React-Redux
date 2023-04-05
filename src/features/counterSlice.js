6b25f135e674d843b83b96e18519648c60d2eacd
Kristen von Gerichten <kristenvongerichten@Kristens-MBP.lan>
2023-04-05 18:41:42 -0400

    added comments

--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -1,28 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+// Define the initial state of the counter slice
 const initialState = {
     value: 0
 }
 
+// Create a counter slice using the createSlice function
 export const counterSlice = createSlice({
+    // Set the name of the slice to 'counter'
     name: 'counter',
+    // Set the initial state of the slice
     initialState,
+    // Define the reducers for the slice
     reducers: {
+        // Define the increment reducer
         increment: (state) => {
+            // Return a new state object with the value property incremented by 1
             return { value: state.value + 1 }
         },
+        // Define the decrement reducer
         decrement: (state) => {
+            // Return a new state object with the value property decremented by 1
             return { value: state.value - 1 }
         },
+        // Define the incrementByAmount reducer
         incrementByAmount: (state, action) => {
+            // Return a new state object with the value property incremented by the payload amount
             return { value: state.value + action.payload }
         }
     }
 })
 
+// Extract the action creators from the slice
 export const { increment, decrement, incrementByAmount } = counterSlice.actions
 
+// Export the reducer function from the slice
 export default counterSlice.reducer
+
 
 
 
--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,25 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+// Define the initial state of the todo slice
 const initialState = {
   items: []
 }
 
+// Create a todo slice using the createSlice function
 export const todoSlice = createSlice({
+  // Set the name of the slice to 'todos'
   name: 'todos',
+  // Set the initial state of the slice
   initialState,
+  // Define the reducers for the slice
   reducers: {
+    // Define the addTodo reducer
     addTodo: (state, action) => {
+      // Add the new todo item to the state's items array
       state.items.push(action.payload)
     },
+    // Define the removeTodo reducer
     removeTodo: (state, action) => {
+      // Remove the todo item with the specified ID from the state's items array
       state.items = state.items.filter(item => item.id !== action.payload)
     },
+    // Define the clearTodos reducer
     clearTodos: state => {
+      // Clear all items from the state's items array
       state.items = []
     }
   }
 })
 
+// Extract the action creators from the slice
 export const { addTodo, removeTodo, clearTodos } = todoSlice.actions
 
+// Export the reducer function from the slice
 export default todoSlice.reducer
+
